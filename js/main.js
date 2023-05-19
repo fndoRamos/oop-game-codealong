bd0d6b515e94cbc1b368a9b9231d1f00dd923331
Fernando Ramos <fndo.ramos@gmail.com>
2023-05-19 10:34:12 +0200

    add before class for game

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Player {
   constructor() {
-    this.positionX = 50;
+    this.width = 6;
+    this.height = 10;
+    this.positionX = 50 - this.width / 2;
     this.positionY = 0;
-    this.width = 20;
-    this.height = 20;
 
     this.domElement = null;
 
@@ -23,30 +23,101 @@ class Player {
   }
 
   moveLeft() {
-    this.positionX--;
-    this.domElement.style.left = this.positionX + "vw";
+    if (this.positionX > 0) {
+      this.positionX -= 1;
+      this.domElement.style.left = this.positionX + "vw";
+    }
     console.log(`new positionX.... ${this.positionX}`);
   }
 
   moveRigth() {
-    this.positionX++;
-    this.domElement.style.left = this.positionX + "vw";
+    if (this.positionX < 100 - this.width) {
+      this.positionX += 1;
+      this.domElement.style.left = this.positionX + "vw";
+    }
     console.log(`new positionX.... ${this.positionX}`);
   }
 
   moveUp() {
-    this.positionY++;
+    if (this.positionY < 90 - this.height) {
+      this.positionY += 2;
+      this.domElement.style.bottom = this.positionY + "vh";
+    }
     console.log(`new positionY.... ${this.positionY}`);
-
   }
 
   moveDown() {
-    this.positionY--;
+    if (this.positionY > 0) {
+      this.positionY -= 2;
+      this.domElement.style.bottom = this.positionY + "vh";
+    }
     console.log(`new positionY.... ${this.positionY}`);
   }
 }
 
+class Obstacle {
+  constructor() {
+    this.width = 10;
+    this.height = 10;
+    this.positionX = Math.floor(Math.random() * (100 - this.width));
+    this.positionY = 85;
+
+    this.domElement = null;
+
+    this.createDomElement();
+  }
+
+  createDomElement() {
+    this.domElement = document.createElement("div");
+
+    this.domElement.className = "obstacle";
+    this.domElement.style.width = this.width + "vw";
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.bottom = this.positionY + "vh";
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.domElement);
+  }
+
+  moveDown() {
+    this.positionY--;
+    this.domElement.style.bottom = this.positionY + "vh";
+    console.log(`new obstacle...`);
+  }
+}
+
 const player = new Player();
+const obstaclesArr = [];
+
+//const obstacleOne = new Obstacle();
+
+setInterval(() => {
+  const newObstacle = new Obstacle();
+  const score = document.querySelector("#points span");
+  const scoreValue = Number(score.innerText);
+  score.innerText = scoreValue + 100;
+  obstaclesArr.push(newObstacle);
+}, 1000);
+
+setInterval(() => {
+  obstaclesArr.forEach((obstacleInstance) => {
+    obstacleInstance.moveDown();
+
+    if (
+      obstacleInstance.positionX < player.positionX + player.width &&
+      obstacleInstance.positionX + obstacleInstance.width > player.positionX &&
+      obstacleInstance.positionY < player.positionY + player.height &&
+      obstacleInstance.height + obstacleInstance.positionY > player.positionY
+    ) {
+      console.log("game over my fren");
+      //location.href = "./gameover.html";
+    }
+    if (  obstacleInstance.positionY < 0 - obstacleInstance.height) {
+      obstacleInstance.domElement.remove()
+      obstaclesArr.shift();
+    }
+  });
+}, 60);
 
 // attach event listeners...
 document.addEventListener("keydown", (e) => {
